5490d277eeeaf5a380bf2214256b285997db8a5f
Svetlana Lap <laptevas80@gmail.com>
2022-09-15 22:18:54 +0300

    add "FirstApp - C# Семинар1

---
+++ FirstApp/Program.cs
@@ -0,0 +1,43 @@
+﻿// Console.WriteLine("Введите число");
+// int a;
+// a = Convert.ToInt32(Console.ReadLine());
+
+// int result = a*a;
+
+// Console.WriteLine("Квадрат числа: " + a*a);
+
+
+
+// int a, b;
+// b = Convert.ToInt32(Console.ReadLine());
+// a = Convert.ToInt32(Console.ReadLine());
+// if (a * a == b){
+//     Console.WriteLine("yes");
+// }
+// else {Console.WriteLine ("No");}
+
+
+
+string a;
+a = Console.ReadLine();
+if (a == "1"){
+    Console.WriteLine("понедельник");
+}
+if (a == "2"){
+    Console.WriteLine("вторник");
+}
+if (a == "3"){
+    Console.WriteLine("среда");
+}
+if (a == "4"){
+    Console.WriteLine("четверг");
+}
+if (a == "5"){
+    Console.WriteLine("пятница");
+}
+if (a == "6"){
+    Console.WriteLine("суббота");
+}
+if (a == "7"){
+    Console.WriteLine("воскресенье");
+}
